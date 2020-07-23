247dc9cf2c962874fa8466f6f7d7540a04c269ae
Ali <mypublicemail911@gmail.com>
2020-07-23 00:06:47 -0600

    times variables changed

--- times.py
+++ times.py
@@ -1,6 +1,6 @@
 def t_sum(times):
 	tlist = [ x.split(":") for x in times ]
-	seconds = sum([ int(x[2]) for x in tlist ])
-	minutes = sum([ int(x[1]) for x in tlist ]) + int(seconds/60)
-	hours = sum([ int(x[0]) for x in tlist ]) + int(minutes/60)
-	return [ hours , minutes % 60 , seconds % 60]
+	s = sum([ int(x[2]) for x in tlist ])
+	m = sum([ int(x[1]) for x in tlist ]) + int(seconds/60)
+	h = sum([ int(x[0]) for x in tlist ]) + int(minutes/60)
+	return [ h , m % 60 , s % 60]
