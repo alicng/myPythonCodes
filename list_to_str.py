16b953be18bf017d488cdfdf97a51a9b2b0ee4e6
Ali <mypublicemail911@gmail.com>
2020-08-24 19:29:53 -0600

    four new code

---
+++ list_to_str.py
@@ -0,0 +1,7 @@
+def a(n):
+    c=''.join(str(i) for i in n)
+    return type(c)
+	
+         
+a=a ([1, 2, 3, 4, 5, 6]) 
+print(a)
---
+++ measure.py
@@ -0,0 +1,6 @@
+def a(n):
+   
+	return str(n).count('[')
+         
+a=a ([[[]]])
+print(a)
