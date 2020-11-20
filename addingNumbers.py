a19c70b313f4ffba66a60fe26671b9fbe26c82aa
Ali <mypublicemail911@gmail.com>
2020-11-19 22:34:43 -0700

    addingNumbers.update

--- addingNumbers.py
+++ addingNumbers.py
@@ -1,6 +1,6 @@
 def add(n1, n2):
 
-    return str(eval(f'{n1} + {n2}')) if n1 and n2 else 'Invalid Operation'
+    return str(eval(f'{n1} + {n2}')) if n1 and n2 else "Invalid Operation"
 
 a=add("-10", "-20")
 print(a)
